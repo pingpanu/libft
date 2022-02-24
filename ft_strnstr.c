4653cedf3d3662ab06db558f635f5768fab29521
pingpanu <dingpanuwong@gmail.com>
2022-02-24 11:53:06 +0700

    corrected ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 10:43:51 by user              #+#    #+#             */
-/*   Updated: 2022/02/24 02:04:03 by user             ###   ########.fr       */
+/*   Updated: 2022/02/24 11:52:43 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,15 @@
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t  n;
-    size_t  i;
 
     n = ft_strlen(needle);
     if (n == 0)
         return ((char*)haystack);
-    i = 0;
-    while (haystack[i] && i < len)
+    while (*haystack && n <= len--)
     {
-        if (!ft_strncmp(&haystack[i], needle, n))
-            return ((char*) (haystack + i));
-        i++;
+        if(!ft_memcmp(haystack,needle,n))
+            return((char*)haystack);
+        haystack++;
     }
     return (NULL);
 }
-/*
-int main()
-{
-    char    haystack[30] = "aaabcabcd";
-    char    *p1;
-    char    *p2;
-    p1 = strnstr(haystack,"cd",8);
-    p2 = ft_strnstr(haystack,"cd",8);
-
-    printf("strnstr result = %s\n",p1);
-    printf("ft_strnstr result = %s\n",p2);
-    return (0);
-}*/
